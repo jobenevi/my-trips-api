03fcd05588381dd232a154d9a51ea65d6b02c117
jobenevi <jonatasmarques09@gmail.com>
2024-10-05 13:54:54 -0300

    added testimonials-home endpoint

---
+++ models/dto/testimonialHomeDTO.go
@@ -0,0 +1,19 @@
+package dto
+
+import "my-trips-api/models"
+
+type TestimonialHomeDTO struct {
+	ID           int    `json:"id"`
+	Author       string `json:"author"`
+	Content      string `json:"content"`
+	ProfileImage string `json:"profile_image"`
+}
+
+func ConvertTestimoninalToTestimonialHomeDTO(testimonial *models.Testimonial) TestimonialHomeDTO {
+	return TestimonialHomeDTO{
+		ID:           int(testimonial.ID),
+		Author:       testimonial.Name,
+		Content:      testimonial.Content,
+		ProfileImage: testimonial.ProfileImage,
+	}
+}
